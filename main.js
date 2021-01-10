1082f91897a34b7d576d5ac546cabd7335f13aa2
Marek Zemla <zemla@marksite4u.pl>
2021-01-10 19:34:04 +0000

    JS update

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-// http://websamuraj.pl/examples/js/projekt11/
+const btnStart = document.querySelector('.main')
+const btnReset = document.querySelector('.reset')
+let divTime = document.querySelector('.time div')
+let time = 0;
+let idInterval;
+
+const change=()=>{
+    if(btnStart.textContent == "Start"){
+     btnStart.textContent = "stop"
+      idInterval = setInterval(start,10);}
+    else {
+         btnStart.textContent = "Start"
+         clearInterval(idInterval)
+     }   
+    
+    }      
+
+const start = ()=>{
+    time++
+    divTime.textContent = (time/100).toFixed(2)
+
+}
+
+const reset =()=>{
+    
+    clearInterval(idInterval)
+    divTime.textContent = "---"
+    btnStart.textContent = "Start"
+}
+ 
+
+btnStart.addEventListener('click',change)
+btnReset.addEventListener('click',reset)
